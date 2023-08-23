7fa345673e845fa0767523fba3f43f5017e5469a
Ehab-24 <ehabs1775@gmail.com>
2023-08-23 20:35:13 +0500

    patch - bigger hero section for small devices

--- src/routes/components/hero.svelte
+++ src/routes/components/hero.svelte
@@ -4,16 +4,16 @@
 </script>
 
 <div
-  class=" w-screen h-[70vh] overflow-x-hidden grid grid-cols-[24] grid-rows-[13] bg-grid"
+  class=" w-screen h-[90vh] md:h-[70vh] overflow-x-hidden grid grid-cols-[24] grid-rows-[13] bg-grid"
   style="mask-image: linear-gradient(to bottom, transparent, black);
   webkit-mask-image: linear-gradient(to bottom, transparent, black);"
 >
   <!-- background -->
-  <div class=" absolute w-screen h-[70vh] overflow-hidden">
+  <div class=" absolute w-screen h-[90vh] md:h-[70vh] overflow-hidden">
     <img
       alt="hero background"
       src={beams}
-      class="opacity-30 w-screen h-[70vh] scale-x-[2] md:scale-x-150 rotate-90 md:rotate-0"
+      class="opacity-30 w-screen h-[90vh] md:h-[70vh] scale-x-[2] md:scale-x-150 rotate-90 md:rotate-0"
     />
   </div>
   <div
@@ -22,10 +22,10 @@
 
   <!-- main content -->
   <main
-    class=" absolute mt-10 md:mt-0 w-screen h-[70vh] flex flex-col gap-32 pb-20"
+    class=" absolute mt-10 px-4 md:px-0 md:mt-0 w-screen h-[90vh] md:h-[70vh] flex flex-col gap-32 pb-20"
   >
     <div
-      class=" flex flex-col gap-12 w-full md:w-2/3 xl:w-2/5 self-center h-full justify-end items-center"
+      class=" flex flex-col gap-12 w-full md:w-2/3 xl:w-2/5 self-center h-full justify-center md:justify-end items-center"
     >
       <h1
         class=" text-7xl font-bold text-center text-zinc-700 dark:text-white tracking-tight"
@@ -35,9 +35,7 @@
         <span class=" text-yellow-500">S</span>tudent
         <span class=" text-green-700">C</span>lub
       </h1>
-      <p
-        class=" px-4 md:px-0 dark:text-[#bababa] text-slate-500 text-center tracking-wide"
-      >
+      <p class=" dark:text-[#bababa] text-slate-500 text-center tracking-wide">
         Unleash innovation with the exceptional Google Developer Student Club at
         <span class=" font-mono font-bold text-green-500 dark:text-green-400"
           >Information Technology University</span
